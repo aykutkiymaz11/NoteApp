d95c2f44d926e5dd6f76465632873a6352f23aab
aykutkiymaz11 <40243034+aykutkiymaz11@users.noreply.github.com>
2025-02-15 14:45:45 +0300

    Delete note app/NoteAppLast directory

--- note app/NoteAppLast/app/src/main/java/com/example/noteapp/feature_note/presentation/notes/NotesState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.noteapp.feature_note.presentation.notes
-
-import com.example.noteapp.feature_note.domain.model.Note
-
-data class NotesState(
-    val notes: List<Note> = emptyList(),
-    val selectedImageUri: String? = null
-)
